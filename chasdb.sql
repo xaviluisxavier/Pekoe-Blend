11179a3fe2e707431d8e610064568b058d6078b4
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2023-01-18 09:26:53 -0100

    Update chasdb.sql

--- chasdb.sql
+++ chasdb.sql
@@ -1,3 +1,19 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema chasdb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema chasdb
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `chasdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `chasdb` ;
 
@@ -27,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `chasdb`.`users` (
   `senha` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 7
+AUTO_INCREMENT = 8
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
